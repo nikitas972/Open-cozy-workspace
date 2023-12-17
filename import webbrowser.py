b8585839fd4eabbc272be3e1da9bade858f3f320
nikitas972 <143893354+nikitas972@users.noreply.github.com>
2023-12-17 17:06:57 +0100

    Add files via upload

---
+++ import webbrowser.py
@@ -0,0 +1,8 @@
+import webbrowser
+import os 
+# open default webbrowser
+webbrowser.open_new("https://www.youtube.com/watch?v=jfKfPfyJRdk") #cozy music
+webbrowser.open_new_tab("https://stackoverflow.com") #stackoverflow for tutorials
+webbrowser.open_new_tab("https://wallpapers.com/search/lofi") #wallpapers for my linux workshop
+
+os.system("cava")
